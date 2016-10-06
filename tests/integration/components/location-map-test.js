0c4ab778bf9086dcf02a9c4fc79e0015be0faaef
wolfieorama <wachirawamunyi@gmail.com>
2016-10-06 03:44:58 +0300

    generated map service and unit test

--- tests/integration/components/location-map-test.js
+++ tests/integration/components/location-map-test.js
@@ -2,8 +2,8 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import Ember from 'ember';
 
-let StubMapService = Ember.Service.extend({
-  getMapElement(location){
+let StubMapsService = Ember.Service.extend({
+  getMapElement(location) {
     this.set('calledWithLocation', location);
     // We create a div here to simulate our maps service,
     // which will create and then cache the map element
@@ -13,18 +13,15 @@ let StubMapService = Ember.Service.extend({
 
 moduleForComponent('location-map', 'Integration | Component | location map', {
   integration: true,
-  beforeEach(){
+  beforeEach() {
     this.register('service:maps', StubMapsService);
     this.inject.service('maps', { as: 'mapsService' });
   }
 });
 
 test('should append map element to container element', function(assert) {
-  this.set('myLocation', 'New York')
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('myLocation', 'New York');
   this.render(hbs`{{location-map location=myLocation}}`);
-
   assert.equal(this.$('.map-container').children().length, 1, 'the map element should be put onscreen');
-  assert.equal(this.get('mapsService.calledWithLocation'), 'New york', 'a map of New York should be requested');
+  assert.equal(this.get('mapsService.calledWithLocation'), 'New York', 'a map of New York should be requested');
 });
